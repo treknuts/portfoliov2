232a92163fcd8e00dbdc25083cadb05ef670493e
treknuts <trevorknutson.complete@gmail.com>
2023-05-18 13:55:28 -0500

    Stopped using useState with loading projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,9 @@
 import "./Projects.css";
 import ProjectCard from "./ProjectCard";
-import { useState } from "react";
 import data from "../projects.json";
 
 const Projects = ({ id }) => {
-    const [ projects, setProjects ] = useState(data);
+    const projects = data;
     return (
         <div id={ id } className="page">
             <h1>Projects</h1>
